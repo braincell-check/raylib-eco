14479da3f10a14c274b99b6366779c6dfd0f4d55
Evan Pratten <ewpratten@gmail.com>
2021-10-12 11:14:31 -0400

    Copy over files

--- src/lib.rs
+++ src/lib.rs
@@ -1,66 +1,78 @@
-#![doc = include_str!("../README.md")]
-#![deny(unsafe_code)]
-#![warn(
-    clippy::all,
-    clippy::await_holding_lock,
-    clippy::char_lit_as_u8,
-    clippy::checked_conversions,
-    clippy::dbg_macro,
-    clippy::debug_assert_with_mut_call,
-    clippy::doc_markdown,
-    clippy::empty_enum,
-    clippy::enum_glob_use,
-    clippy::exit,
-    clippy::expl_impl_clone_on_copy,
-    clippy::explicit_deref_methods,
-    clippy::explicit_into_iter_loop,
-    clippy::fallible_impl_from,
-    clippy::filter_map_next,
-    clippy::float_cmp_const,
-    clippy::fn_params_excessive_bools,
-    clippy::if_let_mutex,
-    clippy::implicit_clone,
-    clippy::imprecise_flops,
-    clippy::inefficient_to_string,
-    clippy::invalid_upcast_comparisons,
-    clippy::large_types_passed_by_value,
-    clippy::let_unit_value,
-    clippy::linkedlist,
-    clippy::lossy_float_literal,
-    clippy::macro_use_imports,
-    clippy::manual_ok_or,
-    clippy::map_err_ignore,
-    clippy::map_flatten,
-    clippy::map_unwrap_or,
-    clippy::match_on_vec_items,
-    clippy::match_same_arms,
-    clippy::match_wildcard_for_single_variants,
-    clippy::mem_forget,
-    clippy::mismatched_target_os,
-    clippy::mut_mut,
-    clippy::mutex_integer,
-    clippy::needless_borrow,
-    clippy::needless_continue,
-    clippy::option_option,
-    clippy::path_buf_push_overwrite,
-    clippy::ptr_as_ptr,
-    clippy::ref_option_ref,
-    clippy::rest_pat_in_fully_bound_structs,
-    clippy::same_functions_in_if_condition,
-    clippy::semicolon_if_nothing_returned,
-    clippy::string_add_assign,
-    clippy::string_add,
-    clippy::string_lit_as_bytes,
-    clippy::string_to_string,
-    clippy::todo,
-    clippy::trait_duplication_in_bounds,
-    clippy::unimplemented,
-    clippy::unnested_or_patterns,
-    clippy::unused_self,
-    clippy::useless_transmute,
-    clippy::verbose_file_reads,
-    clippy::zero_sized_map_values,
-    future_incompatible,
-    nonstandard_style,
-    rust_2018_idioms
-)]
+/* raylib-rs
+   lib.rs - Main library code (the safe layer)
+
+Copyright (c) 2018-2019 Paul Clement (@deltaphc)
+
+This software is provided "as-is", without any express or implied warranty. In no event will the authors be held liable for any damages arising from the use of this software.
+
+Permission is granted to anyone to use this software for any purpose, including commercial applications, and to alter it and redistribute it freely, subject to the following restrictions:
+
+  1. The origin of this software must not be misrepresented; you must not claim that you wrote the original software. If you use this software in a product, an acknowledgment in the product documentation would be appreciated but is not required.
+
+  2. Altered source versions must be plainly marked as such, and must not be misrepresented as being the original software.
+
+  3. This notice may not be removed or altered from any source distribution.
+*/
+
+//! # raylib-rs
+//!
+//! `raylib` is a safe Rust binding to [Raylib](https://www.raylib.com/), a C library for enjoying games programming.
+//!
+//! To get started, take a look at the [`init_window`] function. This initializes Raylib and shows a window, and returns a [`RaylibHandle`]. This handle is very important, because it is the way in which one accesses the vast majority of Raylib's functionality. This means that it must not go out of scope until the game is ready to exit. You will also recieve a !Send and !Sync [`RaylibThread`] required for thread local functions.
+//!
+//! For more control over the game window, the [`init`] function will return a [`RaylibBuilder`] which allows for tweaking various settings such as VSync, anti-aliasing, fullscreen, and so on. Calling [`RaylibBuilder::build`] will then provide a [`RaylibHandle`].
+//!
+//! Some useful constants can be found in the [`consts`] module, which is also re-exported in the [`prelude`] module. In most cases you will probably want to `use raylib::prelude::*;` to make your experience more smooth.
+//!
+//! [`init_window`]: fn.init_window.html
+//! [`init`]: fn.init.html
+//! [`RaylibHandle`]: struct.RaylibHandle.html
+//! [`RaylibThread`]: struct.RaylibThread.html
+//! [`RaylibBuilder`]: struct.RaylibBuilder.html
+//! [`RaylibBuilder::build`]: struct.RaylibBuilder.html#method.build
+//! [`consts`]: consts/index.html
+//! [`prelude`]: prelude/index.html
+//!
+//! # Examples
+//!
+//! The classic "Hello, world":
+//!
+//! ```
+//! use raylib::prelude::*;
+//!
+//! fn main() {
+//!     let (mut rl, thread) = raylib::init()
+//!         .size(640, 480)
+//!         .title("Hello, World")
+//!         .build();
+//!     
+//!     while !rl.window_should_close() {
+//!         let mut d = rl.begin_drawing(&thread);
+//!         
+//!         d.clear_background(Color::WHITE);
+//!         d.draw_text("Hello, world!", 12, 12, 20, Color::BLACK);
+//!     }
+//! }
+//! ```
+#![cfg_attr(feature = "nightly", feature(optin_builtin_traits))]
+#![allow(dead_code)]
+pub mod consts;
+pub mod core;
+pub mod ease;
+pub mod prelude;
+pub mod rgui;
+pub mod rlights;
+
+/// The raw, unsafe FFI binding, in case you need that escape hatch or the safe layer doesn't provide something you need.
+pub mod ffi {
+    pub use raylib_sys::*;
+}
+
+pub use crate::core::collision::*;
+pub use crate::core::file::*;
+pub use crate::core::logging::*;
+pub use crate::core::misc::{get_random_value, open_url};
+pub use crate::core::*;
+
+#[cfg(feature="serde")]
+extern crate serde;
